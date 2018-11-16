b5d706c6fa8196d22d45b47594f4f4f063febf2a
paloke <paloke@gmail.com>
2018-11-16 13:59:25 +0100

    Spanish translation.

---
+++ l10n/es.js
@@ -0,0 +1,21 @@
+OC.L10N.register(
+  "files_rightclick", {
+    "Open folder" : "Abrir carpeta",
+    "Open file" : "Abrir archivo",
+    "Open in new tab" : "Abrir en una nueva pestaña",
+    "Edit file" : "Editar archivo",
+    "Read PDF" : "Leer PDF",
+    "See picture" : "Ver imagen",
+    "Open in Gallery" : "Abrir en la Galería",
+    "Play" : "Reproducir",
+    "Stop playback" : "Parar reproducción",
+    "Watch" : "Visualizar",
+    "Share folder" : "Compartir carpeta",
+    "Share file" : "Compartir archivo",
+    "Select" : "Seleccionar",
+    "Unselect" : "Deseleccionar",
+  "Get WebDAV link" : "Obtener el enlace de WebDAV",
+  "Copied !": "¡Copiado!"
+  },
+  "nplurals=2; plural=(n > 1);"
+);
